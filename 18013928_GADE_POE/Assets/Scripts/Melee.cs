e6824c13e65396e47471d0c9975b6729f6f9b302
Deatherlavish <deathergamer123@gmail.com>
2019-10-26 20:24:04 +0200

    units moving and spawning test
    made units spawn from red team and blue team

---
+++ 18013928_GADE_POE/Assets/Scripts/Melee.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Melee : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject BlueSword;
+    public Sprite blueMeleeSprite;
+    public Sprite redMeleeSprite;
+    private SpriteRenderer spriteRenderer;
+    float speed = 0.5f;
+    string faction;
+   public float yMovement;
+    
+
+   public float xMovement;
+    int teamSelect;
+    float nextdirection;
+   public float nextDirectionChangeRate;
+    float randDirection;
+    private Rigidbody2D rb;
+    private Vector2 movement = new Vector2();
+    void Start()
+    {
+        teamSelect = Random.Range(0, 3);   //TEST
+        if(teamSelect == 1)
+        {
+            faction = "Blue";
+        }
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        if(spriteRenderer.sprite == null)
+        {
+            spriteRenderer.sprite = blueMeleeSprite;
+        }
+        rb = this.GetComponent<Rigidbody2D>();
+       if(faction == "Blue")
+        {
+            spriteRenderer.sprite = blueMeleeSprite;
+        }
+        else
+        {
+            spriteRenderer.sprite = redMeleeSprite;
+        }
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > nextdirection)
+        {
+            nextdirection = Time.time + nextDirectionChangeRate;
+
+           
+            nextDirectionChangeRate = Random.Range(1, 5);
+            randDirection = Random.Range(0, 9);
+            if(randDirection == 1)
+            {
+                xMovement = -1;
+                yMovement = 0;
+            }
+            else if(randDirection == 2)
+            {
+                xMovement = 1;
+                yMovement = 0;
+            }
+            else if (randDirection == 3)
+            {
+                xMovement = 0;
+                yMovement = -1;
+            }
+            else if (randDirection == 4)
+            {
+                xMovement = 0;
+                yMovement = 1;
+            }
+            else if (randDirection == 5)
+            {
+                xMovement = 1;
+                yMovement = 1;
+            }
+            else if (randDirection == 6)
+            {
+                xMovement = -1;
+                yMovement = 1;
+            }
+            else if (randDirection == 7)
+            {
+                xMovement = -1;
+                yMovement = -1;
+            }
+            else if (randDirection == 8)
+            {
+                xMovement = 1;
+                yMovement = -1;
+            }
+            
+        }
+        if (transform.position.x > 8.6)
+        {
+            //Destroy(BlueSword);
+            xMovement = -1;
+        }
+        if (transform.position.x < -11.7)
+        {
+            // Destroy(BlueSword);
+            xMovement = 1;
+        }
+        if(transform.position.y > 8)
+        {
+            yMovement = -1;
+        }
+        if (transform.position.y < -12.4)
+        {
+            yMovement = 1;
+        }
+        transform.position += new Vector3(xMovement, yMovement) *Time.deltaTime;
+    }
+}
---
+++ 18013928_GADE_POE/Assets/Scripts/Respawn.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+    public GameObject swordsmen;
+    public GameObject archers;
+    public float randX;
+    public float randY;
+    float respawnSpeed;
+    Vector2 spawnlocations;
+    public float spawnRate = 4;
+    float nextspawn = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.time > nextspawn)
+        {
+            nextspawn = Time.time + spawnRate;
+            randX = Random.Range(-4, 4);  //move to spawneer
+            randY = Random.Range(-4, 4);  //move to spawner
+            spawnlocations = new Vector2(randX,randY);
+            Instantiate(swordsmen, spawnlocations,Quaternion.identity);
+        }
+    }
+}
